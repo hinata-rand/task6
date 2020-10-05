4fdb70aba4fd6fae1eb1a426e78fbb150fb2c94d
hinata-rand <hinata333san@gmail.com>
2020-10-05 02:15:48 +0000

    アプリパス修正

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,15 @@ Route::get('/', function () {
     return redirect('tasks');
 })->middleware('auth');
 
+Route::middleware('verified')->group(function () {
+    Route::group(['middleware' => 'auth:user'], function () {
+        // メール認証済みかつログイン済みのユーザーが見れる画面
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
+});
+
 Route::resource('tasks', 'TaskController');
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
